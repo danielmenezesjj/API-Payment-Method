f87d49dcc3ee9466bb36bf2d13aaf75a58e6ffdd
Daniel Menezes <danielmenezes2512@gmail.com>
2023-07-13 16:51:27 -0300

    documentação pronta

--- src/index.js
+++ src/index.js
@@ -4,11 +4,43 @@ const app = express();
 const path = require("path");
 const port = 3001;
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json');
+const productsDocument = require('./productsDocument.json');
+const pixDocument = require('./pix.json')
+const authDocument = require('./auth.json')
 
+
+
+
+
+
+const combinedDocument = {
+    ...swaggerDocument,
+    paths: {
+      ...authDocument.paths,
+      ...swaggerDocument.paths,
+      ...productsDocument.paths,
+      ...pixDocument.paths
+    }
+  };
 
 app.set("view engine", "ejs");
 app.use(cors());
 app.set("views", path.join(__dirname, "views"));
-app.listen(port, () => console.log(`A api está rodando na porta ${port}`));
 routes(app);
+const swaggerOptions = {
+  swaggerOptions: {
+    requestInterceptor: (req) => {
+      // Adicionar o token JWT no cabeçalho "Authorization"
+      req.headers['Authorization'] = 'Bearer SEU_TOKEN_JWT';
+      return req;
+    },
+  },
+};
+
+// Rota para a documentação do Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(combinedDocument, swaggerOptions));
+
+app.listen(port, () => console.log(`A api está rodando na porta ${port}`));
 module.exports = app;
